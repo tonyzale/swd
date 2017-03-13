ad9fd4ae3beccaf611c468fd12e503ad016f82d6
tonyzale <tonyzale@gmail.com>
2017-03-13 01:12:08 +0000

    socket service

--- client/js/client_game.js
+++ client/js/client_game.js
@@ -1,98 +1,101 @@
 (function(angular) {
     var gameApp = angular.module('GameApp', []);
-    gameApp.controller('ChatController', ['$scope', 'modalService', function($scope, modalService) {
-        /*global io*/
-        var socket = io.connect();
-
+    gameApp.controller('ChatController', ['$scope', 'modalService', 'socketService', function($scope, modalService, socketService) {
         $scope.messages = [];
         $scope.roster = [];
         $scope.name = '';
         $scope.text = '';
-        
+
         $scope.moves = [];
         $scope.modalService = modalService;
 
-        socket.on('connect', function() {
+        socketService.socket.on('connect', function() {
             $scope.setName();
         });
 
-        socket.on('message', function(msg) {
+        socketService.socket.on('message', function(msg) {
             $scope.messages.push(msg);
             $scope.$apply();
         });
 
-        socket.on('roster', function(names) {
+        socketService.socket.on('roster', function(names) {
             $scope.roster = names;
             $scope.$apply();
         });
 
-        socket.on('state', function(state) {
+        socketService.socket.on('state', function(state) {
             $scope.game_state = JSON.parse(state);
             $scope.$apply();
         });
 
-        socket.on('moves', function(moves) {
+        socketService.socket.on('moves', function(moves) {
             $scope.moves = JSON.parse(moves);
             $scope.clearMovesFromCards();
-            $scope.game_state.player.forEach(function (p){
-               p.hand.forEach(function(c){
-                   c.moves = $scope.movesForCard(c);
-               });
-               p.characters.forEach(function(char){
-                   char.card.moves = $scope.movesForCard(char.card);
-                   char.upgrades.forEach(function(u){
-                       u.card.moves = $scope.movesForCard(u.card);
-                   });
-               });
+            $scope.game_state.player.forEach(function(p) {
+                p.hand.forEach(function(c) {
+                    c.moves = $scope.movesForCard(c);
+                });
+                p.characters.forEach(function(char) {
+                    char.card.moves = $scope.movesForCard(char.card);
+                    char.upgrades.forEach(function(u) {
+                        u.card.moves = $scope.movesForCard(u.card);
+                    });
+                });
             });
             $scope.$apply();
         });
 
         $scope.send = function send() {
             console.log('Sending message:', $scope.text);
-            socket.emit('message', $scope.text);
+            socketService.socket.emit('message', $scope.text);
             $scope.text = '';
         };
 
         $scope.setName = function setName() {
-            socket.emit('identify', $scope.name);
+            socketService.socket.emit('identify', $scope.name);
         };
-        
+
         $scope.clearMovesFromCards = function() {
-            $scope.game_state.player.forEach(function (p){
-               p.hand.forEach(function(c){
-                   c.moves = [];
-               });
+            $scope.game_state.player.forEach(function(p) {
+                p.hand.forEach(function(c) {
+                    c.moves = [];
+                });
             });
         };
-        
+
         $scope.movesForCard = function(card) {
             return $scope.moves.filter(function(m) {
-                return (m.card && (m.card.id == card.id)); 
+                return (m.card && (m.card.id == card.id));
             });
         };
     }]);
     gameApp.directive('card', ['modalService', function(modalService) {
-       return {
-           restrict: 'E',
-           scope: {
-               card: '=info',
-               left: '=left',
-               top: '=top'
+        return {
+            restrict: 'E',
+            scope: {
+                card: '=info',
+                left: '=left',
+                top: '=top'
             },
-           templateUrl: 'card.html',
-           link: function(scope) {
-               scope.clickCard = function() {
-                   modalService.clickedCard = scope.card;
-                   modalService.showModal = true;
-               };
-           }
-       };
+            templateUrl: 'card.html',
+            link: function(scope) {
+                scope.clickCard = function() {
+                    modalService.clickedCard = scope.card;
+                    modalService.showModal = true;
+                };
+            }
+        };
     }]);
-    gameApp.factory('modalService', function(){
+    gameApp.factory('modalService', function() {
         return {
             showModal: false,
             clickedCard: undefined
+        };
+    });
+    gameApp.factory('socketService', function() {
+        return {
+            /*global io*/
+            socket: io.connect()
         };
     });
 })(window.angular);
