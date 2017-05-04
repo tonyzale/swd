a4371f400a0e3cb5d5238e0b961a9c5c61aa753d
Tony Zale <tonyzale@gmail.com>
2017-05-03 21:24:39 -0500

    upgrade socket,io dep

--- server.ts
+++ server.ts
@@ -27,93 +27,83 @@ let game = new destiny.GameState(p1, p2);
 let router = express();
 let server = http.createServer(router);
 let game_io = socketio.listen(server);
-(<any>game_io).set('log level',2);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 let messages = [];
 let sockets = [];
 
-game_io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-    
-    socket.emit('state', JSON.stringify(game));
-    socket.emit('moves', JSON.stringify(
-      game.GetAvailableActions(0).map(function(v){
-        return v.serialized;
-      })));
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      let text = String(msg || '');
-
-      if (!text)
-        return;
-
-      (<any>socket).get('name', function (err, name) {
-        let data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      (<any>socket).set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-    
-    socket.on('choice', function(choice) {
-      console.log('Got choice: ' + choice);
-      socket.emit('modal', JSON.stringify({
-        id: 'gotchoice',
-        title: 'Received',
-        text: 'Got your choice of ' + choice
-      }));
-    });
-
-    socket.on('move-selection', function(move) {
-      (<any>socket).get('name', function(err, name) {
-        let data = {
-          name: name,
-          text: 'Played: ' + move.name
-        };
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
+interface GameSocket extends SocketIO.Socket {
+  name: string;
 }
 
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
+game_io.on('connection', function(socket: GameSocket) {
+  messages.forEach(function(data) {
+    socket.emit('message', data);
   });
-}
 
-server.listen((<any>process.env).PORT || 3000, (<any>process.env).IP || "0.0.0.0", function(){
+  socket.emit('state', JSON.stringify(game));
+  socket.emit('moves', JSON.stringify(
+    game.GetAvailableActions(0).map(function(v) {
+      return v.serialized;
+    })));
+
+  sockets.push(socket);
+
+  socket.on('disconnect', function() {
+    sockets.splice(sockets.indexOf(socket), 1);
+    updateRoster();
+  });
+
+  socket.on('message', function(msg) {
+    if (!msg)
+      return;
+    let data = { name: socket.name, text: msg };
+    broadcast('message', data);
+    messages.push(data);
+  });
+
+  socket.on('identify', function(name) {
+    socket.name = name || 'Anonymous';
+  });
+
+  socket.on('choice', function(choice) {
+    console.log('Got choice: ' + choice);
+    socket.emit('modal', JSON.stringify({
+      id: 'gotchoice',
+      title: 'Received',
+      text: 'Got your choice of ' + choice
+    }));
+  });
+
+  socket.on('move-selection', function(move) {
+    let data = {
+      name: socket.name,
+      text: 'Played: ' + move.name
+    };
+    broadcast('message', data);
+    messages.push(data);
+  });
+
+  function updateRoster() {
+    async.map(
+      sockets,
+      function(socket, callback) {
+        callback(socket.name);
+      },
+      function(err, names) {
+        broadcast('roster', names);
+      }
+    );
+  }
+
+  function broadcast(event, data) {
+    sockets.forEach(function(socket) {
+      socket.emit(event, data);
+    });
+  }
+});
+server.listen((<any>process.env).PORT || 3000, (<any>process.env).IP || "0.0.0.0", function() {
   let addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
+
