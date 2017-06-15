1d515e6600522e85a0b937d5d114f848bd808253
Tony Zale <tonyzale@gmail.com>
2017-06-14 21:23:51 -0500

    use tsc from node modules

--- build_all.sh
+++ build_all.sh
@@ -1,2 +1,2 @@
-tsc --outDir client/js client_ts/client_game.ts
-tsc server.ts
+node_modules/typescript/bin/tsc --outDir client/js client_ts/client_game.ts
+node_modules/typescript/bin/tsc server.ts
